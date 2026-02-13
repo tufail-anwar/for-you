8c504292d3847d633274303a4822aa8a1c329468
MD TUFAIL ANWAR <dildarmr887@gmail.com>
2026-02-14 00:11:07 +0530

    Update memory card images and letters in scripts.js

--- scripts.js
+++ scripts.js
@@ -31,24 +31,24 @@ memoBtn.addEventListener("click", () => {
   memoContainer.innerHTML = `
   <div class="memory-box">
 
-    <div class="memory-card">
-      <img src="${imgPath}memory-image1.jpg">
-      <p>Every day feels magical with you ❤️</p>
+    <div class="memory-card ">
+      <img src="${imgPath}image1.jpeg">
+      <p class="p-1" >Jb hm mile the roadster mai tb ki pic hai .pic to acchi nhi hai but hmari phli kiss isi din hui thi❤️</p>
     </div>
 
     <div class="memory-card">
-      <img src="${imgPath}memory-image2.jpg">
-      <p>Your smile is my favorite view 😊</p>
+      <img src="${imgPath}image2.jpeg">
+      <p class="p-2">Is bar jb hm aye the to app dusre resturant mai gyi saste k wjh se chle gye the. kitna chota tha nhh but bhi fir bhi appni harkatoo se baz nhi a rhe the 😂 or appne isi din mujhe phle chappar mara tha 😊</p>
     </div>
 
     <div class="memory-card">
-      <img src="${imgPath}memory-image3.jpg">
-      <p>Together forever, no matter what 💕</p>
+      <img src="${imgPath}image3.jpeg">
+      <p class="p-3">Ye tb ki pic hai .jb hm appse last bar mile the appko chorne ka mnn nhi kr rha tha .mnn kr rha pkr rkhe kahi nhi jane de 🥺</p>
     </div>
 
      <div class="memory-card">
-      <img src="${imgPath}memory-image3.jpg">
-      <p>Together forever, no matter what 💕</p>
+      <img src="${imgPath}image4.jpeg">
+      <p class="p-4">Inhi ankho de drten hai hmm but pyaar bhi inhi se krte hai  😘💕</p>
     </div>
 
   </div>
@@ -72,10 +72,7 @@ memoContainer.addEventListener("click", (e) => {
 const letter=document.querySelector('.js-letter')
 const letterContainer=document.querySelector('.js-letter--container')
 letter.addEventListener("click", () => {
-  letterContainer.innerHTML=`<div class="letter-box js-box">Hello! Madam jii <p>❤️Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem animi eum accusantium in voluptas totam eos ipsum dolorem perspiciatis est, inventore laborum, obcaecati vitae nesciunt ullam nemo facere suscipit dolore.
-        Numquam sequi, aut quibusdam illo mollitia beatae eveniet ab eligendi deserunt repellat voluptates dicta animi recusandae a aliquid dolore atque autem assumenda rem consequatur! Voluptate possimus fugiat autem adipisci iste!
-        Dolor minima a enim repellat? In doloribus perspiciatis eum ad magnam repellendus necessitatibus cupiditate. Dolore delectus necessitatibus eius laudantium at deserunt provident sapiente tempore a, blanditiis, adipisci maxime obcaecati repudiandae?
-        Repellat corporis molestiae aliquid atque quibusdam architecto nam rem consectetur, impedit accusantium vel debitis. Unde quis et obcaecati consectetur? Impedit omnis quis harum eum maxime animi rerum, iure nemo iusto!    💕</p>From Your Love ❤️</div>`;
+  letterContainer.innerHTML=`<div class="letter-box js-box">Hello! Madam jii <p>❤️ 5 sal ho gye or pta bhi chala .ajj kl key relationships mahine bhr main tut jate hai . hm itte sal se sth hai hmari biuch ldai bhi hui ,mahino bhr baat bnd rha but appke dil mere liye pyaar kmm nhi hua .Mujhe toh phle viswas hi nhi tha k koi mere jaise ldke ko itta pyaar kr skta hai. hmm ittna pyaar deserve nhi krte the but app meri aukat se jyda pyaar kiya . mai itta bura ldka hun , itti pyaari ldki hurt kr deta hun ,rula deta hun but mai bhi apse boht pyaar krta hun lnk appna pyaar dikha nhi pata .mere liye app boht important (sbse jyda important).Appke liye mai hmm kch bhi kr skata hun . I lOVE U Sooooo Much baccha . main appke bina  kch nhi 🥺.Please app kbhi chor k mt jana . jaan maine appko Valentine week main bhi rula diya , meri wjh se apki tabyat bhi bigad gyi .I am so sorry baccha .app jo kahogi wo karunga but aise gussa mt raho mere se.    💕</p></div>`;
   letterContainer.classList.add("show");
   pageContent.classList.add("blur");
 
